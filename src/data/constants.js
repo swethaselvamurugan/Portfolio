d3c02a6baa20ea86c88f44f338bda389788dff02
swethaselvamurugan <swetha040725@gmail.com>
2025-02-21 12:32:08 +0530

    Added

---
+++ src/data/constants.js
@@ -0,0 +1,393 @@
+import CodeHydride from '../images/Code-Hydride-Logo.png';
+import Jpr from '../images/Jeppiaar-Logo.png';
+import MEI from '../images/MEI-Logo.jpg';
+import StMarys from '../images/St.Marys-Logo.png';
+import AItools from '../images/AI-Tools.png';
+import Canva from '../images/Canva.png';
+import EMC from '../images/EMC-Logo.jpg';
+import FullStack from '../images/Full-Stack.png';
+import ProjectManagement from '../images/Project-Management.png';
+import Simplilearn from '../images/Simplilearn-Logo.jpg';
+import Coursera from '../images/Coursera-Logo.jpg';
+import Google from '../images/Google-Logo.jpg';
+import be10x from '../images/be10x-Logo.png';
+import Accenture from '../images/Accenture-Logo.jpg';
+import Excel from '../images/Excel.png';
+import GenerativeAI from '../images/GenerativeAI.png';
+import Python from '../images/Python.png';
+import TripAdvisor from '../images/TripAdvisor.png';
+import Nostra from '../images/Nostra.png';
+import Greenden from '../images/Greenden.png';
+import WeatherApp from '../images/WeatherApp.png';
+import Actodo from '../images/Actodo.png';
+import BulkMail from '../images/BulkMail.png';
+import BlogPortfolio from '../images/BlogProject.png';
+import JokesGenerator from '../images/JokesGenerator.png';
+import QuickCart from '../images/QuickCart.png';
+import MEIIntern from '../images/MEI-Intern.jpg';
+// import CodeHydrideIntern from '../images/CodeHydride-Intern.png';
+
+export const Bio = {
+  name: "Swetha",
+  roles: [
+    "Full Stack Web Developer",
+    "MERN Stack Developer"
+  ],
+  description:
+    "Fresher IT student and full-stack developer skilled in the MERN stack (MongoDB, Express.js, React, Node.js) with experience in frontend/backend development, API integration, and CRUD operations. I’m eager to apply my skills in a collaborative, growth-oriented environment where I can continue to learn and innovate.",
+  github: "https://github.com/swethaselvamurugan",
+  resume:
+    "https://drive.google.com/file/d/14Qaf4GnsgR_MrGBoPhFYcL-Sua4Qkjxl/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/swetha-full-stack-mern/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image:
+          "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+      },
+      {
+        name: "Javascript",
+        image:
+          "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "https://img.icons8.com/?size=100&id=CIAZz2CYc6Kc&format=png&color=000000",
+      },
+      {
+        name: "React",
+        image:
+          "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://img.icons8.com/?size=100&id=b6vIINYN0kfW&format=png&color=000000",
+      },
+      {
+        name: "Responsive Web Design",
+        image:
+          "https://img.icons8.com/?size=100&id=M6Ps6biUL9KD&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node.Js",
+        image: "https://img.icons8.com/?size=100&id=54087&format=png&color=000000",
+      },
+      {
+        name: "Express.Js",
+        image: "https://img.icons8.com/?size=100&id=2ZOaTclOqD4q&format=png&color=000000"
+      },
+      {
+        name: "MySQL",
+        image: "https://img.icons8.com/?size=100&id=UFXRpPFebwa2&format=png&color=000000",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://img.icons8.com/?size=100&id=bosfpvRzNOG8&format=png&color=000000",
+      },
+      {
+        name: "RESTful APIs",
+        image:
+          "https://img.icons8.com/?size=100&id=21895&format=png&color=000000",
+      },
+      {
+        name: "CRUD",
+        image: "https://img.icons8.com/?size=100&id=pEAInx3HGXxB&format=png&color=FFFCFC",
+      },
+      {
+        name: "User Authentication",
+        image:
+          "https://img.icons8.com/?size=100&id=cPrjfwMAWOhO&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    skills: [
+      {
+        name: "Adaptability",
+        image:
+          "https://img.icons8.com/?size=100&id=VDEOwtgJxQLe&format=png&color=000000",
+      },
+      {
+        name: "Project Management",
+        image:
+          "https://img.icons8.com/?size=100&id=02IKaup4G9QZ&format=png&color=000000",
+      },
+      {
+        name: "Teamwork",
+        image:
+          "https://img.icons8.com/?size=100&id=4oZkN406pMPK&format=png&color=000000",
+      },
+      {
+        name: "Communication",
+        image:
+          "https://img.icons8.com/?size=100&id=117509&format=png&color=000000",
+      },
+      {
+        name: "Time Management",
+        image:
+          "https://img.icons8.com/?size=100&id=4TN1A3VHK7II&format=png&color=000000",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+      },
+      {
+        name: "Git",
+        image:
+          "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        "name": "TypeScript",
+        "image": "https://img.icons8.com/?size=100&id=nCj4PvnCO0tZ&format=png&color=000000"
+      },
+      {
+        name: "Problem-solving (Solving problems on platforms like Leetcode, GeeksforGeeks)",
+        image:
+          "https://img.icons8.com/?size=100&id=MdqtGh8jocCe&format=png&color=000000",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: CodeHydride,
+    certificate: ProjectManagement,
+    // certificate: CodeHydrideIntern,
+    role: "Intern",
+    company: "Code Hydride",
+    date: "Aug 2024 - Feb 2025",
+    desc: "I worked as an intern at Code Hydride, where I contributed to a real-time e-learning platform built using the MERN stack. I got to explore tools like Asana, TypeScript, and Shadcn, which helped me level up my technical skills. Beyond coding, I also gained valuable experience in handling MOM (Minutes of Meeting), maintaining email etiquette, and keeping track of project timelines.",
+    skills: [
+      "Asana",
+      "Shadcn",
+      "TypeScript",
+      "Github"
+    ],
+    doc: "https://drive.google.com/file/d/1MOIz0oHbV-ADI1d5NbfrjLIxEKTZS6kC/view?usp=sharing",
+  },
+  {
+    id: 1,
+    img: MEI,
+    certificate: MEIIntern,
+    role: "Intern",
+    company: "Madras Engineering Industries",
+    date: "5 Mar 2024 - 21 Mar 2024",
+    desc: "During my internship at Madras Engineering Industries, I focused on building a strong foundation in Python. I worked on enhancing my problem-solving skills, learning key concepts and techniques that helped me tackle coding challenges more effectively.",
+    skills: [
+      "Python",
+      "Problem-solving",
+    ],
+    doc: "https://drive.google.com/file/d/1VI5NmjUNXcph4diADNLfqsyFoqCvnIkC/view?usp=sharing",
+  },
+];
+
+export const certifications = [
+  {
+    id: 0,
+    img: EMC,
+    certificate: FullStack,
+    role: "Full Stack Developer (MERN)",
+    company: "Error Makes Clever",
+    date: "Completed on 22 Mar 2024",
+    doc: "https://drive.google.com/file/d/12zsH_m4YdYRgxdOGOElop4QpASlWTXq1/view?usp=sharing",
+  },
+  {
+    id: 1,
+    img: Google,
+    certificate: GenerativeAI,
+    role: "Introduction to Generative AI",
+    company: "Google",
+    date: "Completed on 30 Jan 2025",
+    doc: "https://drive.google.com/file/d/15PsS3YIEJrytAIneqD0Sz1e3fWAul5an/view?usp=sharing",
+  },
+  {
+    id: 2,
+    img: Coursera,
+    certificate: ProjectManagement,
+    role: "Introduction to Project Management",
+    company: "Coursera",
+    date: "Completed on 16 Nov 2024",
+    doc: "https://drive.google.com/file/d/19fGxWFdM3pSSn2X_XL4t1zyvQSjEj-yF/view?usp=sharing",
+  },
+  {
+    id: 3,
+    img: Accenture,
+    certificate: Python,
+    role: "Python",
+    company: "Accenture",
+    date: "Completed on 6 Jan 2025",
+    doc: "https://drive.google.com/file/d/11s3AOVBCPqpCqUBuGbHKGjqPnnzPYPIj/view?usp=sharing",
+  },
+  {
+    id: 4,
+    img: Simplilearn,
+    certificate: Canva,
+    role: "Canva for Beginners",
+    company: "Simplilearn",
+    date: "Completed on 25 Nov 2024",
+    doc: "https://drive.google.com/file/d/19tK2pSmC0Cc8TDZVFP0W12sF0-dBypFO/view?usp=sharing",
+  },
+  {
+    id: 5,
+    img: Coursera,
+    certificate: Excel,
+    role: "Introduction to Microsoft Excel",
+    company: "Coursera",
+    date: "Completed on 6 Jan 2025",
+    doc: "https://drive.google.com/file/d/1HK0McqafJwH1DhfeAjx2MQWITwDhSy1K/view?usp=sharing",
+  },
+  {
+    id: 6,
+    img: be10x,
+    certificate: AItools,
+    role: "AI tools Workshop",
+    company: "be10x",
+    date: "Completed on 9 Apr 2024",
+    doc: "https://drive.google.com/file/d/1BPF2Zqevf4cKIGMMTDiP-gpRROQ4ZzLZ/view?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: Jpr,
+    school: "Jeppiaar Engineering College, Chennai.",
+    date: "2022 - 2026",
+    grade: "8.3 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology. I have completed 5 semesters and I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, System Design, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: StMarys,
+    school: "St. Mary’s Matric. Hr. Sec. School, Cuddalore.",
+    date: "2022",
+    grade: "89.67%",
+    desc: "I completed my class 12 high school education at St. Mary's School, Cuddalore, with a focus on Mathematics, Physics, Chemistry and Biology.",
+    degree: "HSC - 12th Grade",
+  },
+  {
+    id: 2,
+    img: StMarys,
+    school: "St. Mary’s Matric. Hr. Sec. School, Cuddalore.",
+    date: "2020",
+    grade: "90.8%",
+    desc: "I completed my class 10 education at St. Mary's School, Cuddalore, with a focus on Mathematics, Science and Social Science.",
+    degree: "SSC - 10th Grade",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "QuickCart",
+    description: "Developed a full-fledged MERN-stack e-commerce platform with admin and customer views, secure role-based authentication, payment processing, and cloud storage for product images. The project is designed to handle both the frontend and backend seamlessly for a complete shopping experience.",
+    image: QuickCart,
+    tags: ["MongoDB", "Express Js", "React", "Node Js", "Redux", "Cloudinary", "Paypal Developer"],
+    github: "https://github.com/swethaselvamurugan/QuickCart.git",
+    webapp: "https://quickcart-1-myul.onrender.com",
+  },
+  {
+    id: 1,
+    title: "Bulkmail",
+    description: "Developed a MERN-stack app that integrates Nodemailer to send bulk emails to recipients listed in an Excel sheet. This tool automates email campaigns and makes managing communications easier.",
+    image: BulkMail,
+    tags: ["MongoDB", "Express Js", "React", "Node Js", "NodeMailer"],
+    github: "https://github.com/swethaselvamurugan/BulkMail.git",
+    webapp: "https://bulkmail-1-swa9.onrender.com",
+  },
+  {
+    id: 2,
+    title: "Blog Website",
+    description: "Built a secure blogging platform where only a designated user can post blogs while others can read them. Implemented role-based access control and Firebase authentication to ensure secure and personalized user experience.",
+    image: BlogPortfolio,
+    tags: ["MongoDB", "Express Js", "React", "Node Js", "Firebase Authentication"],
+    github: "https://github.com/swethaselvamurugan/Blog-Website.git",
+    webapp: "https://blog-website-1-baz0.onrender.com",
+  },
+  {
+    id: 3,
+    title: "Actodo",
+    description: "Built a task management app that lets users create and delete their daily to-do list. Features include user authentication, weather updates, and a dynamic date display, all built with React, Tailwind CSS, and JavaScript.",
+    image: Actodo,
+    tags: ["React", "TailwindCSS", "Javascript"],
+    github: "https://github.com/swethaselvamurugan/Actodo.git",
+    webapp: "https://actodo-black-rho.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Jokes Generator",
+    description: "Built a simple React app using Redux that fetches and displays random jokes from the Chuck Norris API. This project was a fun way to learn and implement Redux state management while delivering a humorous user experience.",
+    image: JokesGenerator,
+    tags: ["React", "Redux", "ChuckNorris API", "Javascript"],
+    github: "https://github.com/swethaselvamurugan/Jokes-Generator.git",
+    webapp: "https://jokes-generator-ruby.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Weather App",
+    description: "Developed a weather forecasting app using React that integrates with the OpenWeatherMap API. The app allows users to fetch weather reports for any city in real time, providing a sleek and responsive interface.",
+    image: WeatherApp,
+    tags: ["React", "OpenWeatherMap API", "Javascript"],
+    github: "https://github.com/swethaselvamurugan/Weather-API.git",
+    webapp: "https://weather-api-zeta-taupe.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Greenden",
+    description: "Built a visually appealing online store dedicated to plants using HTML and Tailwind CSS. The site was designed to be fully responsive, ensuring a great shopping experience on both mobile and desktop devices.",
+    image: Greenden,
+    tags: ["HTML", "Tailwind CSS", "Javascript"],
+    github: "https://github.com/swethaselvamurugan/Greenden.git",
+    webapp: "https://greenden-opal.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Nostra",
+    description: "Developed a feature-rich e-commerce platform with a sleek and modern UI, incorporating product filtering, smooth scrolling effects, and easy navigation to enhance the user experience. Built with HTML, CSS, JavaScript, and Media Queries to ensure responsiveness.",
+    image: Nostra,
+    tags: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/swethaselvamurugan/Nostra.git",
+    webapp: "https://nostra-beta.vercel.app/",
+  },
+  {
+    id: 8,
+    title: "TripAdvisor",
+    description: "Created a responsive travel website inspired by TripAdvisor, designed to seamlessly adjust across various screen sizes using HTML, CSS, and Media Queries. Focused on providing an intuitive and user-friendly experience.",
+    image: TripAdvisor,
+    tags: ["HTML", "CSS", "Media Queries"],
+    github: "https://github.com/swethaselvamurugan/TripAdvisor.git",
+    webapp: "https://trip-advisor-lemon.vercel.app/",
+  },
+];
